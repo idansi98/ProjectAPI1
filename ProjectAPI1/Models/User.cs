f56efe5aa4931084f7718efd07cf769ea91d4c13
idansi98 <idansi98@gmail.com>
2023-04-09 15:05:24 +0300

    Added UserName + Password fields

--- ProjectAPI1/Models/User.cs
+++ ProjectAPI1/Models/User.cs
@@ -8,4 +8,8 @@ public partial class User
     public int Number { get; set; }
 
     public string? UserId { get; set; }
+
+    public string? UserName { get; set; }
+
+    public string? Password { get; set; }
 }
